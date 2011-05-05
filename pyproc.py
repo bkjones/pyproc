da8ebd65eb8969400c67a01f636054de124e9b49
Brian K. Jones <bkjones@gmail.com>
2011-05-05 07:59:03 -0700

    Added function to convert little-endian hex IPs to dotted-quad IPs.

--- pyproc.py
+++ pyproc.py
@@ -1,5 +1,7 @@
 import os
 import pwd
+import socket
+import struct
 
 PROCDIR = '/proc'
 
@@ -15,12 +17,22 @@ def get_pids_by_uname(uname):
 
     return pids
 
+def ip_from_le_hex(le_hex):
+    """
+    le_hex is a little-endian hex representation of an IP address, 
+	a format used by the C library and found in (for our case) 
+	/proc/net/tcp. This returns a dotted-quad IP address. 
+
+    """
+    laddr = int(le_hex, 16)
+    packed_ip = struct.pack("<L", laddr)
+    ip = socket.inet_ntoa(packed_ip)
+    return ip
+
 def get_open_files_by_pid(pid):
+    pass
     
 
 
 if __name__ == '__main__':
-    puid = get_pids_by_uid(1000)
-    pname = get_pids_by_uname('jonesy')
-    print "By UID: %s" % puid
-    print "By Name: %s" % pname
+    assert ip_from_le_hex('881210AC') == '172.16.18.136'
